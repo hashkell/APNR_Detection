6ec29a6e405db4c4efa5381c8a4e9453775a51f1
hashkell <hashkellz@gmail.com>
2019-08-30 00:58:50 +0530

    creating a local copy of images from json file

--- input_data.py
+++ input_data.py
@@ -15,31 +15,49 @@ formats = [
 ]
 
 image_key = ''
+annotation_key = ''
 
 
 def parse_name(img_path):
     '''
     parses  a given image file img_path, a URL or Localpath
     returns a tuple containing image's path, file_name
-    and a boolean (True if image has allowed file format)
-
+    args :
+        img_path : a url to image or local path
     '''
+
     path, file = os.path.split(img_path)
-    for f in formats:
-        if f in file:
-            return path, file, True
-    return path, file, False
+    return path, file
 
 
 def gen_data_from_json(feature_type='nd', target_vector='yolo'):
-    with open(dat_file_path) as f:
-        for line in f:
-            data = json.loads(line)
+    records_read = 0
+    with list(open(dat_file_path)) as f:
+        for line in f[records_read:]:
+            data = json.loads(line.rstrip())
             img_path = data[image_key]
             try:
-                img_attribs = parse_name(img_path)
+                path, file = parse_name(img_path)
+                file_names_split = file.split('.')
+                root_name = file_names_split[0]
+                extension = file_names_split[-1]
 
-                img = request.urlopen(img_path)
-                pass
+                img = request.urlopen(img_path).read()
+
+                with open(file, 'wb') as f:
+                    f.write(img)
+                with open(root_name+'.txt', 'w') as f:
+                    annotations = data[annotation_key]
+
+                    for annotation in annotations:
+                        annotation_string = ''
+                        f.write(annotation_string)
+                records_read += 1
             except e:
-                pass
+                summary = str(
+                    {
+                        'records_read': records_read,
+                    }
+                )
+                with open('summary.txt', 'w') as f:
+                    f.write(summary)
